bfa3c6b6e944b25f8c989d1f50c956e36edf727d
Arkadiusz Górka <arekgorka91@gmail.com>
2022-10-20 11:31:35 +0200

    first commit

---
+++ src/main/java/com/kantorfrontend/KantorFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kantorfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KantorFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KantorFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kantorfrontend/KantorFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kantorfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KantorFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
